f9bd288d63a26a241023cd2941119d24684e3dc1
Carlos Daniel Barros <barros.carlosds@gmail.com>
2020-02-15 02:50:56 -0300

    full funcional

--- main.js
+++ main.js
@@ -1,37 +1,110 @@
 class Task {
-  constructor(content, date) {
-    this.content = content;
-    this.date = date;
-    this.isfinished = false;
-  }
-  finishTask() {
-    this.isfinished = True;
-  }
+    constructor(name, date) {
+        this.name = name;
+        this.date = date;
+        this.isFinished = false;
+        // this.id = create_UUID();
+    }
+
+    finishTask() {
+        this.isFinished = True;
+    }
 }
 
-var tasks = JSON.parse(
-    localStorage.getItem('task_list')
+
+// get task_list from storage
+const tasks = JSON.parse(
+    localStorage.getItem("task_list")
 ) || [];
 
-function addTask(task) {
-  let content = task.split("em");
-  let newTask = new Tarefa(contet[0], content[1]);
-  localStorage.setItem(newTask.content, newTask.date);
+// save task_list on storage
+function saveToStorage() {
+    localStorage.setItem(
+        "task_list", JSON.stringify(tasks)
+    );
 }
 
-function removeTask(task) {
-  localStorage.removeItem(task.conteudo);
+// Criar o obj Task, add obj Task na tasks list,
+// chama a func de rederizar e salva novos dados no storage
+document.getElementById('btn_salvar').onclick = function() {
+    let task_input = document.getElementById('task_name');
+    let input_value = task_input.value;
+
+    if(input_value.length !== 0) {
+        let [name, date] = input_value.split("em");
+        let newTask = new Task(name, date);
+
+        tasks.push(newTask);
+        saveToStorage();
+        task_input.value = '';
+        renderTaskList();
+    }
+
+}
+
+// function addTask(task) {
+//   let name = task.split("em");
+//   let newTask = new Tarefa(contet[0], name[1]);
+//   localStorage.setItem(newTask.name, newTask.date);
+// }
+
+function renderTaskList() {
+    let tbody_element = document.querySelector("#tabela_tarefa tbody");
+    let tr_element = document.createElement('tr');
+    tbody_element.innerHTML = '';
+
+    if (tasks.length < 1) {
+        console.log('nenhuma task encontrada');
+
+        let td_empty = document.createElement('td');
+        td_empty.setAttribute('colspan', 3);
+        td_empty.appendChild(
+            document.createTextNode('Nenhuma tarefa pendente encontrada.')
+        );
+        tr_element.appendChild(td_empty);
+        tbody_element.appendChild(tr_element);
+
+        return;
+    }
+
+    console.log('iniciando processo de renderização');
+
+    for ( let i=0; i < tasks.length; i++) {
+        tr_element = document.createElement('tr');
+
+        let [td_entrega, td_name, td_actions] = createTdElements(tasks[i], i);
+
+        tr_element.appendChild(td_entrega);
+        tr_element.appendChild(td_name);
+        tr_element.appendChild(td_actions);
+
+        tbody_element.appendChild(tr_element);
+    }
+    console.log('fim da renderização de elementos');
+}
+
+renderTaskList();
+
+function removeTask(task_id) {
+    console.log(`remove task: ${tasks[task_id].name}`);
+    tasks.splice(task_id, 1);
+    renderTaskList();
+    saveToStorage();
+}
+
+function concludeTask(task_id) {
+    let task = tasks[task_id];
+    console.log(`conclude task: ${tasks[task_id].name}`);
+
+    if (task) task.isFinished = true;
+    console.log(task.isFinished);
+    renderTaskList();
+    saveToStorage();
 }
 
 function getTasks() {
   return localStorage;
 }
 
-// SALVANDO NO STORAGE
-function saveToStorage() {
-  localStorage.setItem("task_list", JSON.stringify(tasks));
-}
 
-function renderTaskList() {
-    let 
-}
+// trabalho de lpweb em 20/02/2020
---
+++ utils.js
@@ -0,0 +1,96 @@
+// UTILS
+
+function createTdElements({name, date, isFinished}, task_id) {
+    const td_list = new Array;
+
+    td_list.push(createTdEntrega(date));
+    td_list.push(createTdNome(name, isFinished));
+    td_list.push(createTdActions(task_id, isFinished));
+
+    return td_list;
+}
+
+function createTdEntrega(date=null) {
+    let [day, month] = date.split('/');
+
+    let td_entrega = document.createElement('td');
+    td_entrega.appendChild(
+        document.createTextNode(`${day}/${month}`)
+    );
+
+    return td_entrega;
+}
+
+function createTdNome(name, finished) {
+    let td_name = document.createElement('td');
+
+    if (finished) {
+        let del_element = document.createElement('del');
+        del_element.appendChild(
+            document.createTextNode(name)
+        )
+        td_name.appendChild(del_element);
+    } else {
+        td_name.appendChild(
+            document.createTextNode(name)
+        );
+    }
+
+    return td_name;
+}
+
+function createTdActions(task_id, finished) {
+    let td_actions = document.createElement('td');
+
+    const btn_div = document.createElement('div');
+    btn_div.classList.add('text-center', 'align-middle', 'btn-group');
+
+    // create btn save
+    let btn_save = document.createElement('button');
+    btn_save.classList.add('btn', 'btn-sm', 'btn-outline-success');
+    btn_save.setAttribute( 'onclick', `concludeTask(${task_id})` );
+    
+    let li_save = document.createElement('li');
+    li_save.classList.add('fas', 'fa-check');
+    
+    btn_save.appendChild(li_save);
+    
+    // create btn delete
+    let btn_delete = document.createElement('button');
+    btn_delete.classList.add('btn', 'btn-sm', 'btn-outline-danger');
+    btn_delete.setAttribute( 'onclick', `removeTask(${task_id})` );
+    
+    let li_delete = document.createElement('li');
+    li_delete.classList.add('fas', 'fa-trash-alt');
+    btn_delete.appendChild(li_delete);
+
+    // disable btns for isFinished == true
+    if (finished) {
+        console.log('tarefa concluida, desabilitando botões de ação');
+        btn_save.setAttribute('disabled', 'true');
+        btn_delete.setAttribute('disabled', 'true');
+    }
+
+    // add btns on btn_div
+    btn_div.appendChild(btn_save);
+    btn_div.appendChild(btn_delete);
+
+    // add btn_div on td_actions
+    td_actions.appendChild(btn_div);
+
+    return td_actions;
+}
+
+function createSpinnerElement() {
+    let span_element = document.createElement('span');
+    span_element.classList.add('sr-only');
+    span_element.appendChild(
+        document.createTextNode('Carregando...')
+    );
+
+    const div_element = document.createElement('div');
+    div_element.classList.add('spinner-border');
+    div_element.appendChild(span_element);
+
+    return div_element;
+}
